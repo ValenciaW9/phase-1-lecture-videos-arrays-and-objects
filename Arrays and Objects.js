af57e2970c3e45b9c26af152407ae5fa1e0b9593
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-06 15:36:58 -0400

    phase-1-lecture-videos-arrays-and-objects/
    phase-1-lecture-videos-arrays-and-objects/

---
+++ Arrays and Objects.js
@@ -0,0 +1,57 @@
+
+//Video : Arrays and Objects
+//Arrays vs Objects
+//Array
+[]
+List like
+Access values through index
+[index]
+iterable(easy to loop over)
+Mutable
+let letters = ['a,','b','c','d', 'e']
+
+let letters let letters: string[]
+console.log (letters)[0])
+console.log (letters)[1])
+console.log (letters)[2])
+
+let letters =[ 'a', 'b' ,'c']
+letters[0] ='z'
+letters[1] = true
+letters[3] = 1
+console.log(letters)
+
+let book = {
+    title ={
+        title ;'You don\t know js',
+        author: 'Kyle Simpson'
+        publisher 'O\ReillyMedia'
+    }
+}
+book,title = 'Eloquent Javascript'
+book ['author'] = 'Marjin Haverbeke'
+console.log(book)
+
+//Objects
+()
+Reresents something
+Key value pairs 
+Access values through keys
+object key
+iterable(limited)
+Mutable(can be changed)
+let book = {
+    title: ' you don't know js',
+    author: 'Kyle Simpson',
+    publisher: D\'Really Media'
+}
+
+
+let book  = {
+    title: 'You don't know js'
+    author: 'Kyle Simpson',
+    oublisher: 'O\ Reiley Media'
+
+}
+
+
---
+++ phase-1-lecture-videos-arrow-functions-main/function print(book){.js
@@ -0,0 +1,12 @@
+function print(book){
+   return book
+}
+
+const printBookArrow = (book, author) =>[] book , author]
+      return book
+
+
+
+
+ printBook('Eloquent Javascript')
+ printBookArrow('You don\'t know js')
